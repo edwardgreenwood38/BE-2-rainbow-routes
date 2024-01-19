8f4723989db3a3f4068484aa0779bf0bfa9650ce
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-18 20:46:26 -0500

    step 4 completed

--- index.js
+++ index.js
@@ -17,5 +17,19 @@ app.get('/', function (req, res) {
 });
 
 
+// Teal Page
+app.get('/teal', function (req, res) {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: teal;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: teal;">Teal Page</h1>
+        </body>
+    `);
+});
+
+
+
 app.listen(process.env.PORT);
 
