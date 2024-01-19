b1746f6db983677aadf5fc396e98f3d9f43bfdf9
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-18 20:44:32 -0500

    step 3 completed

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+require('dotenv').config();
+
 
 const app = express();
 
 
 app.get('/', function (req, res) {
-    res.send('Hello world!');
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1>White Page</h1>
+        </body>
+    `);
 });
 
 
-app.listen(3000);
+app.listen(process.env.PORT);
 
