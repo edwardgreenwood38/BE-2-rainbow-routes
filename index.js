091a427846675b49b588a26afb5df97b44d6f571
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-21 09:45:13 -0500

    step 6 completed

--- index.js
+++ index.js
@@ -18,26 +18,14 @@ app.get('/', function (req, res) {
 
 
 // Teal Page
-app.get('/teal', function (req, res) {
+app.get('/:color', function (req, res) {
+    const myColor = req.params.color;
     res.send(`
         <body style="margin: 0;">
-            <div style="border: 1px solid black; height: 10vh; background-color: teal;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
                 <h2 style="text-align: center;">NAV BAR</h2>
             </div>
-            <h1 style="color: teal;">Teal Page</h1>
-        </body>
-    `);
-});
-
-
-// crimson Page
-app.get('/crimson', function (req, res) {
-    res.send(`
-        <body style="margin: 0;">
-            <div style="border: 1px solid black; height: 10vh; background-color: crimson;">
-                <h2 style="text-align: center;">NAV BAR</h2>
-            </div>
-            <h1 style="color: crimson;">Crimson Page</h1>
+            <h1 style="color: ${myColor};">${myColor} Page</h1>
         </body>
     `);
 });
